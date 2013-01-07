19deaf20716453d38c4947a2ba2cbf77ba7ba892
Nathan Breit <nabreit@gmail.com>
2013-01-07 18:33:25 -0500

    Fixing session bug

--- boilerplate/Sessions.js
+++ boilerplate/Sessions.js
@@ -4,27 +4,29 @@ function($,        Backbone,   _) {
 
         localStorage: new Backbone.LocalStorage("interview-sessions"),
         
-        toJSON: function(){
-            var attrs = _.clone(this.attributes);
-            if(attrs.startTime) {
-                attrs.startTime = String(attrs.startTime);
+        model: Backbone.Model.extend({
+            toJSON: function(){
+                var attrs = _.clone(this.attributes);
+                if(attrs.startTime) {
+                    attrs.startTime = String(attrs.startTime);
+                }
+                if(attrs.endTime) {
+                    attrs.endTime = String(attrs.endTime);
+                }
+                return attrs;
+            },
+    
+            parse: function(attrs) {
+                //Parse dates strings into js objects
+                if(attrs.startTime) {
+                    attrs.startTime = new Date(attrs.startTime);
+                }
+                if(attrs.endTime) {
+                    attrs.endTime = new Date(attrs.endTime);
+                }
+                return attrs;
             }
-            if(attrs.endTime) {
-                attrs.endTime = String(attrs.endTime);
-            }
-            return attrs;
-        },
+        })
 
-        parse: function(attrs) {
-            //Parse dates strings into js objects
-            if(attrs.startTime) {
-                attrs.startTime = new Date(attrs.startTime);
-            }
-            if(attrs.endTime) {
-                attrs.endTime = new Date(attrs.endTime);
-            }
-            return attrs;
-        }
-        
     });
 });
