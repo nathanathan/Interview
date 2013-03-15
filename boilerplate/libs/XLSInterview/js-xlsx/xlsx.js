80c5bde895172043701a7770765cc9b289644144
Nathan Breit <nabreit@gmail.com>
2013-03-15 13:46:46 -0400

    Updating js-xlsx

--- boilerplate/libs/XLSInterview/js-xlsx/xlsx.js
+++ boilerplate/libs/XLSInterview/js-xlsx/xlsx.js
@@ -111,8 +111,8 @@ function parseSheet(data) { //TODO: use a real xml parser
 		var cells = x.substr(x.indexOf('>')+1).split(/<c/);
 		cells.forEach(function(c, idx) { if(c === "") return;
 			c = "<c" + c;
-			if(refguess.s.c > idx) refguess.s.c = idx; 
-			if(refguess.e.c < idx) refguess.e.c = idx;
+			if(refguess.s.c > idx - 1) refguess.s.c = idx - 1; 
+			if(refguess.e.c < idx - 1) refguess.e.c = idx - 1;
 			var cell = parsexmltag((c.match(/<c[^>]*>/)||[c])[0]); delete cell[0];
 			var d = c.substr(c.indexOf('>')+1);
 			var p = {};
@@ -191,7 +191,7 @@ function parseProps(data) {
 			switch(v[i].v) {
 				case "Worksheets": widx = j; p["Worksheets"] = +v[++i]; break;
 				case "Named Ranges": ++i; break; // TODO: Handle Named Ranges
-				default: throw "Unrecognized key in Heading Pairs: " + v[i].v;
+				default: console.error("Unrecognized key in Heading Pairs: " + v[i++].v);
 			}
 		}
 		var parts = parseVector(q["TitlesOfParts"]);
@@ -332,12 +332,10 @@ function parseZip(zip) {
 
 var fs, jszip;
 if(typeof JSZip !== "undefined") jszip = JSZip;
-/*
 if(typeof require !== "undefined") {
 	if(typeof jszip === 'undefined') jszip = require('./jszip').JSZip;
 	fs = require('fs');
 }
-*/
 
 function readSync(data, options) {
 	var zip, d = data;
@@ -389,8 +387,9 @@ function sheet_to_row_object_array(sheet){
 				r: range.s.r
 			})];
 			if(val){
-				if(val.t === "s"){
-					columnHeaders[C] = val.v;
+				switch(val.t) {
+					case "s": case "str": columnHeaders[C] = val.v; break;
+					case "n": columnHeaders[C] = val.v; break;
 				}
 			}
 		}
